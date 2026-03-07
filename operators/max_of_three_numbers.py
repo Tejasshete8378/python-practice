df3424f39631894190b117c23706809b5f6ca62f
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-03-07 19:01:46 +0530

    Added Python program to find maximum of three numbers

---
+++ operators/max_of_three_numbers.py
@@ -0,0 +1,14 @@
+# This function takes three numbers (a, b, c) as input
+# and returns the largest value using a conditional expression
+def find_maximum(a,b,c):
+    return a if a>b and a>c else b if b>c else c
+
+num1 = int(input("Enter First Number:"))
+num2 = int(input("Enter Second Number:"))
+num3 = int(input("Enter Third Number:"))
+
+## Calling the function 
+# and storing the returned maximum value in maximum_value
+maximum_value = find_maximum(num1,num2,num3)
+
+print("Maximum Value:", maximum_value)
