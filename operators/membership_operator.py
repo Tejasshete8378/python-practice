d1c57c542b0e3d34ae32b3d9ac9ecbc108a83f7f
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-03-09 20:00:11 +0530

    Added Membership Operator Program

--- operators/membership_operator.py
+++ operators/membership_operator.py
@@ -1,3 +1,11 @@
 # Program: Demonstration of Membership Operator in Python
 
-# Description: T
+x = "Hello learning Python is very easy!!!"
+
+print("'H' in x:", 'H' in x)
+
+print("'d' in x:", 'd' in x)
+
+print("'d' not in x:", 'd' not in x)
+
+print("'Python' in x:", 'Python' in x)
