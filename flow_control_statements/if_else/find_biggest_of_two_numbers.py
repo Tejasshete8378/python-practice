d2dbea464af6e0bb1d4b716de94e08d9654f1340
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-03-14 23:28:45 +0530

    Added Program to find Biggest of Two Numbers using If Else Block

---
+++ flow_control_statements/if_else/find_biggest_of_two_numbers.py
@@ -0,0 +1,15 @@
+#File Name:  display_command_line_arguments.py
+#Author: Tejas Shete
+#Purpose: To Find Biggest of Two Numbers
+
+print("-------Program to Find Biggest of Two Numbers-------")
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number:"))
+
+if num1>num2:
+    print("Biggest Number is: ", num1)
+
+else:
+    print("Biggest Number is: ", num2)
+
+
