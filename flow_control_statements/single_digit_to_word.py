f308f661c429a8a72cd922fa3f61462f4580dfaf
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-03-15 20:47:12 +0530

    Added Program for Single digit to Word conversion

---
+++ flow_control_statements/single_digit_to_word.py
@@ -0,0 +1,28 @@
+# File Name : single_digit_to_word.py
+# Author : Tejas Shete
+# Purpose : To print word value of single digit entered
+
+print("\n-----Program to Convert Single Digit to Word-----\n")
+
+num = int(input("Enter Single Digit Number: "))
+
+if num==1:
+    print("Entered Digit is One")
+elif num==2:
+    print("Entered Digit is Two")
+elif num==3:
+    print("Entered Digit is Three") 
+elif num==4:
+    print("Entered Digit is Four") 
+elif num==5:
+    print("Entered Digit is Five") 
+elif num==6:
+    print("Entered Digit is Six") 
+elif num==7:
+    print("Entered Digit is Seven") 
+elif num==8:
+    print("Entered Digit is Eight") 
+elif num==9:
+    print("Entered Digit is Nine") 
+else:
+    print("Please Enter a Single Digit between 0 to 9")                              
