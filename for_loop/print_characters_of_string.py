8d84ddd1344aa054310cda940af1175df33e5d8b
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-03-17 21:02:36 +0530

    Added Program to Print the Characters of a Given String

---
+++ for_loop/print_characters_of_string.py
@@ -0,0 +1,10 @@
+# File Name: print_characters_of_string.py
+# Author: Tejas Shete
+# Purpose: To Create a Program to Print the Characters of a Given String
+
+print("\n-----Program to Print the Characters of a Given String-----\n")
+
+text = input("Enter String: ")
+
+for ch in text:
+    print(ch)
