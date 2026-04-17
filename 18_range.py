a6555cfb18ebfa2e1a39b46e241ff1998270872d
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-04-17 14:39:56 +0530

    Add logic of sum of list and list comprehension

--- 18_range.py
+++ 18_range.py
@@ -18,14 +18,9 @@ print('\n','*'*30,"Welcome to the program",'*'*30,'\n')
 start = int(input("Enter Starting Number: "))
 end = int(input("Enter Ending Number: "))
 
-even_number=[]
-odd_number=[]
+even_number=[i for i in range(start,end+1) if i%2 == 0]
+odd_number=[i for i in range(start,end+1) if i%2 != 0]
 
-for i in range(start,end+1):
-    if i%2 == 0:
-        even_number.append(i)
-    else:
-        odd_number.append(i)
 
 print(f"List of Even Numbers from the Range {start,end} provided: {even_number}")
 print(f"List of Odd Numbers from the Range {start,end} provided: {odd_number} ")
@@ -40,15 +35,11 @@ even_sum = 0
 odd_sum = 0
 
 if choice == 1:
-    
-    for i in even_number:
-        even_sum = even_sum + i
-    print(f"Sum of Even Numbers of Range{start,end}: {even_sum}")
+    print(f"Sum of Even Numbers of Range{start,end}: {sum(even_number)}")
 elif choice == 2:
-    
-    for i in odd_number:
-        odd_sum = odd_sum + i
-    print(f"Sum of Odd Numbers of Range{start,end}: {odd_sum}")
+    print(f"Sum of Odd Numbers of Range{start,end}: {sum(odd_number)}")
+else:
+    print("Invalid Choice. Please enter valid choice")
 
 print('\n','*'*30,"Thank You",'*'*30,'\n')
 
