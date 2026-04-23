9b3d946db6c29977b1153acbbbb160a8eb1c8785
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-04-23 14:47:06 +0530

    Improvised logic

--- Programs/20_new.py
+++ Programs/20_new.py
@@ -18,14 +18,26 @@ You must use a list comprehension to filter the even numbers.
 Do not use the built-in sum() function for the sum key; implement your own loop logic.
 Handle the case where the list might be empty to avoid a "division by zero" error when calculating the average.'''
 
-print('\n','*'*30,"Welcome to the Program,'*'*30",'\n')
+print('\n','*'*30,"Welcome to the Program",'*'*30,'\n')
 
 l1 = eval(input("Enter List: "))
 
-l2 = [i for i in l1 if i % 2 == 0]
+if len(l1) != 0:
+    l2 = [i for i in l1 if i % 2 == 0]
+    
+    total = 0
 
-sum = 0
+    for i in l1:
+        total = total + i 
 
-for i in l1:
-    sum = sum + i 
+    avg = total/len(l1)
+
+    data = {'Even Numbers': l2, 'Sum of all Numbers': total, 'Average of all Numbers': avg}
+ 
+    print('\n',data,'\n')
+
+else:
+    print("List is Empty, Please enter List\n")
+    
+print('*'*30,"Thank You",'*'*30)
 
